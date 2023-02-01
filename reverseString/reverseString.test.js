419500474e4fa66407b9c7cddb4818b5c993f3bd
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 16:27:14 -0600

    Create next test for reverseString to see if it returns reversed string

--- reverseString/reverseString.test.js
+++ reverseString/reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require('./reverseString');
 test('Reverse string returns a string', () => {
   expect(typeof reverseString('string')).toBe(typeof 'string');
 });
+
+test('String is reversed', () => {
+  expect(reverseString('string')).toBe('gnirts');
+});
