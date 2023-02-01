d0082b4016f7797ba2e0cdc1dee4e2a2b3387a9f
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 16:37:33 -0600

    Create first test to see if calculator is an object

---
+++ calculator/calculator.js
@@ -0,0 +1,3 @@
+const calculator = () => {};
+
+module.exports = calculator;
---
+++ calculator/calculator.test.js
@@ -0,0 +1,5 @@
+const calculator = require('./calculator');
+
+test('Calculator is an object', () => {
+  expect(typeof calculator).toBe('object');
+});
