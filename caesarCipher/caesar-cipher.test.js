bd27d3adc54a1ac79c04af53cefe8e5ca8869724
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 17:51:48 -0600

    Add test to see if simple sequence of letters are shifted correctly

--- caesarCipher/caesar-cipher.test.js
+++ caesarCipher/caesar-cipher.test.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-undef */
 const caesarCipher = require('./caesar-cipher');
 
 test('Caesar cipher is a function', () => {
-    expect(typeof caesarCipher).toBe('function');
+  expect(typeof caesarCipher).toBe('function');
 });
 
 test('Caesar cipher returns a string', () => {
   expect(typeof caesarCipher('string')).toBe('string');
+});
+
+test('Caesar cipher shifts letters by shift factor correctly', () => {
+  expect(caesarCipher('abcd')).toBe('bcde');
+  expect(caesarCipher('abcd', 5)).toBe('fghi');
 });
