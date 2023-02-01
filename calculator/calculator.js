963410f314d4c276ace3be253883e1a798509910
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 17:34:06 -0600

    Add all operations to calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,8 +1,8 @@
 const Calculator = () => {
   const add = (n1, n2) => n1 + n2;
-  const subtract = () => {};
-  const multiply = () => {};
-  const divide = () => {};
+  const subtract = (n1, n2) => n1 - n2;
+  const multiply = (n1, n2) => n1 * n2;
+  const divide = (n1, n2) => n1 / n2;
   return {
     add, subtract, multiply, divide,
   };
--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -23,3 +23,17 @@ test('Calculator has a division operation', () => {
 test('Add operation returns the sum of two numbers', () => {
   expect(calculator.add(2, 3)).toBe(5);
 });
+
+test('Subtract operation returns the difference of two numbers', () => {
+  expect(calculator.subtract(2, 3)).toBe(-1);
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test('Multiply operation returns the product of two numbers', () => {
+  expect(calculator.multiply(2, 3)).toBe(6);
+});
+
+test('Divide operation returns the quotient of two numbers', () => {
+  expect(calculator.divide(2, 3)).toBeCloseTo(2 / 3);
+  expect(calculator.divide(3, 2)).toBeCloseTo(1.5);
+});
