8959c29e6040b065281794b6b2de5085826cc919
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 16:54:01 -0600

    Create tests to see if calculator object has operation functions

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -3,3 +3,19 @@ const calculator = require('./calculator');
 test('Calculator is an object', () => {
   expect(typeof calculator).toBe('object');
 });
+
+test('Calculator has an add operation', () => {
+  expect(typeof calculator.add).toBe('function');
+});
+
+test('Calculator has a subtract operation', () => {
+  expect(typeof calculator.subtract).toBe('function');
+});
+
+test('Calculator has a multiply operation', () => {
+  expect(typeof calculator.multiply).toBe('function');
+});
+
+test('Calculator has a division operation', () => {
+    expect(typeof calculator.divide).toBe('function');
+  });
