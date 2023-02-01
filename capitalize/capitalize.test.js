cf449f46aa4cd5577ecadb1c15fa113399c53533
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 15:57:36 -0600

    Create tests to see if capitalize function adequately returns string in the correct format

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -3,3 +3,11 @@ const capitalize = require('./capitalize');
 test('Capitalize function returns a string', () => {
   expect(capitalize('string')).toBe('string');
 });
+
+test('Function does not capitalize first letter if it is already capitalized', () => {
+  expect(capitalize('String')).toBe('String');
+});
+
+test('Function capitalizes first letter if lowercased', () => {
+  expect(capitalize('string')).toBe('String');
+});
