101e43ea29141124cc556736928ceb386e94cc76
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 17:58:41 -0600

    Add feature for basic shifting of letters

--- caesarCipher/caesar-cipher.js
+++ caesarCipher/caesar-cipher.js
@@ -1,5 +1,13 @@
-function caesarCipher(string) {
-  return string;
+function caesarCipher(string, shiftFactor = 1) {
+  let encryptedStr = '';
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < string.length; i += 1) {
+    const char = string.charAt(i);
+    let alphabetIdx = alphabet.indexOf(char);
+    alphabetIdx += shiftFactor;
+    encryptedStr += alphabet.charAt(alphabetIdx);
+  }
+  return encryptedStr;
 }
 
 module.exports = caesarCipher;
