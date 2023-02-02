17e870fd2982d122127c9e728245c52c7bb0741d
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 18:03:28 -0600

    Allow for shift wrapping using modulo of 26

--- caesarCipher/caesar-cipher.js
+++ caesarCipher/caesar-cipher.js
@@ -5,7 +5,7 @@ function caesarCipher(string, shiftFactor = 1) {
     const char = string.charAt(i);
     let alphabetIdx = alphabet.indexOf(char);
     alphabetIdx += shiftFactor;
-    alphabetIdx %= shiftFactor;
+    alphabetIdx %= 26;
     encryptedStr += alphabet.charAt(alphabetIdx);
   }
   return encryptedStr;
