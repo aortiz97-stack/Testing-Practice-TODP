a7ebcefcc2145e2b8cd79d1650996dd91312fe3c
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 18:10:37 -0600

    Create test to see if cases are maintained after encryption

--- caesarCipher/caesar-cipher.test.js
+++ caesarCipher/caesar-cipher.test.js
@@ -1,20 +1,29 @@
 /* eslint-disable no-undef */
 const caesarCipher = require('./caesar-cipher');
 
-test('Caesar cipher is a function', () => {
+test('cipher is a function', () => {
   expect(typeof caesarCipher).toBe('function');
 });
 
-test('Caesar cipher returns a string', () => {
+test('cipher returns a string', () => {
   expect(typeof caesarCipher('string')).toBe('string');
 });
 
-test('Caesar cipher shifts letters by shift factor correctly', () => {
+test('cipher shifts letters by shift factor correctly', () => {
   expect(caesarCipher('abcd')).toBe('bcde');
   expect(caesarCipher('abcd', 5)).toBe('fghi');
 });
 
-test('Caesar cipher shifts letters and wraps around with positive shift factor', () => {
+test('cipher shifts letters and wraps around with positive shift factor', () => {
   expect(caesarCipher('wxyz')).toBe('xyza');
   expect(caesarCipher('wxyz', 5)).toBe('bcde');
-})
+});
+
+test('cipher retains the case of characters', () => {
+  expect(caesarCipher('London')).toBe('Mpoepo');
+  expect(caesarCipher('McKinney')).toBe('NdLjoofz');
+});
+
+/*test('cipher can process words with spaces and punctuation', () => {
+  expect(caesarCipher("London's bridge has fallen!"))
+})*/
