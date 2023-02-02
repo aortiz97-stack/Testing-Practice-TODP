1c3fee671eda051db9894dd9d34b7acf144b30a8
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-01 18:12:27 -0600

    Fulfill test requirement for retaining case of characters

--- caesarCipher/caesar-cipher.js
+++ caesarCipher/caesar-cipher.js
@@ -1,8 +1,13 @@
 function caesarCipher(string, shiftFactor = 1) {
   let encryptedStr = '';
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   for (let i = 0; i < string.length; i += 1) {
     const char = string.charAt(i);
+    let alphabet = lowerAlphabet;
+    if (!lowerAlphabet.includes(char)) {
+      alphabet = upperAlphabet;
+    }
     let alphabetIdx = alphabet.indexOf(char);
     alphabetIdx += shiftFactor;
     alphabetIdx %= 26;
