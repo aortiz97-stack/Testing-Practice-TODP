aacba9296408adb468469aaaf9f8d55e9971f18b
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-03 12:44:35 -0600

    Create first test to see if analyzeArray is a function

---
+++ analyzeArray/analyze-array.js
@@ -0,0 +1,3 @@
+function analyzeArray(){}
+
+module.exports = analyzeArray;
---
+++ analyzeArray/analyze-array.test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+const analyzeArray = require('./analyze-array');
+
+test('analyzeArray is a function', () => {
+  expect(typeof analyzeArray).toBe('function');
+});
