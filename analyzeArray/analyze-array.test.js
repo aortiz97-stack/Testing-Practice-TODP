856a76ebaba2e65797d39faf272f485f19bb7ae1
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-03 12:46:10 -0600

    Add test to see if analyzeArray returns an object

--- analyzeArray/analyze-array.test.js
+++ analyzeArray/analyze-array.test.js
@@ -4,3 +4,7 @@ const analyzeArray = require('./analyze-array');
 test('analyzeArray is a function', () => {
   expect(typeof analyzeArray).toBe('function');
 });
+
+test('analyzeArray returns an object', () => {
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6])).toBe('object');
+});
