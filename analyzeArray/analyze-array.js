2ca8021608da149924fe807a27874cc99a1a9c59
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-03 13:09:17 -0600

    Fix function so that it is a factory function that returns an object, and attributes are numbers, not functions

--- analyzeArray/analyze-array.js
+++ analyzeArray/analyze-array.js
@@ -1,11 +1,11 @@
-function analyzeArray(array) {
-  const average = () => array.reduce((prev, cur) => prev + cur, 0) / array.length;
-  const min = () => Math.min(...array);
-  const max = () => Math.max(...array);
-  const length = () => array.length;
+const analyzeArray = (array) => {
+  const average = array.reduce((prev, cur) => prev + cur, 0) / array.length;
+  const min = Math.min(...array);
+  const max = Math.max(...array);
+  const { length } = array;
   return {
     average, min, max, length,
   };
-}
+};
 
 module.exports = analyzeArray;
--- analyzeArray/analyze-array.test.js
+++ analyzeArray/analyze-array.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 const analyzeArray = require('./analyze-array');
 
-test('analyzeArray is a function', () => {
+test('analyzeArray is a factory function ', () => {
   expect(typeof analyzeArray).toBe('function');
 });
 
@@ -10,24 +10,24 @@ test('analyzeArray returns an object', () => {
 });
 
 test('analyzeArray returns an object with attributes it should have', () => {
-  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe('function');
-  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe('function');
-  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe('function');
-  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe('function');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe('number');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe('number');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe('number');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe('number');
 });
 
 test('analyzeArray returns an object.average returns average of array contents', () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average()).toBe(4);
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
 });
 
 test('analyzeArray returns an object.average returns average of array contents', () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6]).min()).toBe(1);
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe(1);
 });
 
 test('analyzeArray returns an object.average returns average of array contents', () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6]).max()).toBe(8);
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe(8);
 });
 
 test('analyzeArray returns an object.average returns average of array contents', () => {
-  expect(analyzeArray([1, 8, 3, 4, 2, 6]).length()).toBe(6);
+  expect(analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe(6);
 });
