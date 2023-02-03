0e9623e1ddb55b50c5cc72660cc8303c6cf1a889
Armando <armando.ortiz.jr.19@outlook.com>
2023-02-03 12:53:28 -0600

    Add test to see if object has attributes it should have

--- analyzeArray/analyze-array.test.js
+++ analyzeArray/analyze-array.test.js
@@ -8,3 +8,10 @@ test('analyzeArray is a function', () => {
 test('analyzeArray returns an object', () => {
   expect(typeof analyzeArray([1, 8, 3, 4, 2, 6])).toBe('object');
 });
+
+test('analyzeArray returns an object with attributes it should have', () => {
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe('function');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).min).toBe('function');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).max).toBe('function');
+  expect(typeof analyzeArray([1, 8, 3, 4, 2, 6]).length).toBe('function');
+});
